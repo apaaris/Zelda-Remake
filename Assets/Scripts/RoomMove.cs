3f1e491f9eae78655445027739cbb71398b4d463
apaaris <64897205+apaaris@users.noreply.github.com>
2020-12-12 01:03:59 +0100

    Added Text

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
 	public Vector2 cameraChange;
 	public Vector3 playerChange;
 	private CameraMovement cam;
+	public bool needText;
+	public string placeName;
+	public GameObject text;
+	public Text placeText;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +31,21 @@ public class RoomMove : MonoBehaviour
    	 	cam.minPosition += cameraChange;
    	 	cam.maxPosition += cameraChange;
    	 	other.transform.position += playerChange;
+   	 	if(needText)
+   	 	{
+   	 		StartCoroutine(placeNameCo());
+
+   	 	}
 
    	 }
 
+   }
+
+   private IEnumerator placeNameCo()
+   {
+   	text.SetActive(true);
+    placeText.text = placeName;
+    yield return new WaitForSeconds(4f);
+    text.SetActive(false);
    }
 }
